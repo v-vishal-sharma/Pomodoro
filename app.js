c11b0a3ca3651c8e91dbd20957908711e79b5834
Vishal Sharma <vishals200415@gmail.com>
2022-11-29 23:23:26 +0530

    .

--- app.js
+++ app.js
@@ -1,39 +1,20 @@
-// var countDownDate = new Date("May 20, 2022 22:03:00").getTime();
-
-// // Update the count down every 1 second
-// var x = setInterval(function() {
-
-//   // Get today's date and time
-//   var now = new Date().getTime();
-    
-//   // Find the distance between now and the count down date
-//   var distance = countDownDate - now;
-    
-//   // Time calculations for days, hours, minutes and seconds
-//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-//   // Output the result in an element with id="demo"
-//   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-//   + minutes + "m " + seconds + "s ";
-    
-//   // If the count down is over, write some text 
-//   if (distance < 0) {
-//     clearInterval(x);
-//     document.getElementById("demo").innerHTML = "EXPIRED";
-//   }
-// }, 1000);
-
 // Date and Time
+const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
 const date = new Date();
 
 var x = setInterval(function(){
   let hour = date.getHours();
+  let hours = hour>=12 ? hour%12 : hour;
+  let ampm = hour>=12 ? "pm" : "am";
   let minute = date.getMinutes();
-  document.getElementById("hour").innerHTML = hour;
-  document.getElementById("minute").innerHTML = minute;
+  
+  let datee = date.getDate();
+  var month = date.getMonth();
+  month = months[month];
+  let year = date.getFullYear();
+
+  document.getElementById("dTime").innerHTML = hours + ":" + minute + ampm + ", " + datee + " " + month + " " + year;
 },1000);
 
 
@@ -97,5 +78,13 @@ function newElement() {
     }
   }
 
-
 }
+
+//"Enter" adds the item to the list
+const enterForm = document.querySelector(".inputForm");
+enterForm.addEventListener("keydown" , function(e) {
+  if(e.key == "Enter"){
+    newElement();
+  };
+
+});
